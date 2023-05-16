1c9aefff972cecbec6bc15de8692396c2fe020db
Bhaskar Patidar <114685242+macBhaskar@users.noreply.github.com>
2023-05-16 19:43:36 +0530

    Add files via upload

---
+++ supplychainmanagementapp/SupplyChainManagementApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supplychainmanagementapp;
+
+/**
+ *
+ * @author bhaskar patidar
+ */
+public class SupplyChainManagementApp {
+
+    public static void main(String[] args) {
+        MainPage home =new MainPage();
+        home.setVisible(true);
+        
+    }
+}
